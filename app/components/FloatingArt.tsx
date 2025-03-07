44f5e2983b7bdf83bba463361f71412335da6514
nizar7zak <nizar7zakout@gmail.com>
2025-03-07 20:51:13 +0200

    edit floating art

--- app/components/FloatingArt.tsx
+++ app/components/FloatingArt.tsx
@@ -1,17 +1,12 @@
 "use client";
-import { useSectionStore } from "@/lib/store";
 import ArtSide from "@/public/Union.svg";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
-const specialSections = ["الخدمات-التسويقية", "الخدمات-الإبداعية", "الخدمات-التقنية"];
 
 const FloatingArt = () => {
-  const { activeSection } = useSectionStore();
-  const isSpecial = specialSections.includes(activeSection);
-
   return (
-    <div className={ `fixed top-0 h-screen overflow-hidden flex flex-col ${isSpecial ? "left-0" : "right-0"}` }>
+    <div className={ `fixed top-0 h-screen overflow-hidden flex flex-col right-0` }>
       <motion.div
         className="flex flex-col"
         animate={ { y: ["0%", "-50%"] } }
