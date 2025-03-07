a5ecaa5813cfae57151d426967a1e08eeeaad760
nizar7zak <nizar7zakout@gmail.com>
2025-03-07 21:23:17 +0200

    edit text-size

--- app/components/Description.tsx
+++ app/components/Description.tsx
@@ -7,11 +7,10 @@ const Description = ( { text }: Props ) => {
     const { activeSection } = useSectionStore();
 
     const isSpecialSection = ["الخدمات-التسويقية", "الخدمات-الإبداعية", "الخدمات-التقنية"].includes(activeSection);
-    // const textColor = isSpecialSection ? "text-secondary" : "text-white";
     const textJustify = isSpecialSection ? "text-center" : "text-justify";
     
     return (
-        <h1 className={`text-white font-normal xl:font-medium ${textJustify} md:text-[0.8rem] xl:text-[1rem] 2xl:text-[1.4rem]`}>{ text }</h1>
+        <h1 className={`text-white font-normal ${textJustify} md:text-[0.8rem] xl:text-[1rem]  `}>{ text }</h1>
     )
 }
 
