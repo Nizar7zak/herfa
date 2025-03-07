bbb9b5cdaeb752328535dde8a874c3688ae60b4a
nizar7zak <nizar7zakout@gmail.com>
2025-03-07 21:35:19 +0200

    edit line height

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -12,7 +12,7 @@ const Card = ( { text, srcImage, isService }: Props ) => {
 
     return (
         <div className={ `${bgColor} 
-        w-[13vw] h-[25vh] xl:h-[29vh] 2xl:w-[11vw] 2xl:h-[32vh]  rounded-lg flex flex-col justify-center items-center gap-5 xl:gap-8 2xl:gap-6 py-8 `}>
+        w-[13vw] h-[25vh] xl:h-[29vh] 2xl:w-[11vw] 2xl:h-[32vh] 3xl:w-[13vw] 3xl:h-[35vh] rounded-lg flex flex-col justify-center items-center gap-5 xl:gap-8 2xl:gap-6 3xl-gap-8 py-8 `}>
             <Image alt="icon How We Work" src={ srcImage } width={ 30 } height={ 30 } className=" w-[65%] xl:w-[75%] 2xl:w-[70%] h-auto" unoptimized />
             <h1 className={`text-sm xl:text-base  text-center ${color}  w-[90%]  flex-1`}>{ text }</h1>
         </div>
