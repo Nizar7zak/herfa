1546c9824c7d13c1bb1790c57a85e8f018013da0
nizar7zak <nizar7zakout@gmail.com>
2025-03-13 19:20:29 +0200

    edit responsive

--- app/components/FloatingArt.tsx
+++ app/components/FloatingArt.tsx
@@ -6,7 +6,24 @@ import Image from "next/image";
 
 const FloatingArt = () => {
   return (
-    <div className={ `fixed top-0 h-screen overflow-hidden flex flex-col right-0` }>
+    <>
+      <div className={ `fixed top-0 h-screen overflow-hidden hidden lg:flex flex-col right-0` }>
+        <motion.div
+          className="flex flex-col"
+          animate={ { y: [ "0%", "-50%" ] } }
+          transition={ {
+            duration: 50,
+            repeat: Infinity,
+            repeatType: "loop",
+            ease: "linear",
+          } }
+        >
+          <Image alt="زخرفة 1" src={ ArtSide } />
+          <Image alt="زخرفة 2" src={ ArtSide } className={ "-my-3" } />
+        </motion.div>
+      </div>
+
+      <div className={ `fixed -top-2 -right-10 h-screen flex lg:hidden flex-col rotate-90 origin-top-left` }>
       <motion.div
         className="flex flex-col"
         animate={ { y: ["0%", "-50%"] } }
@@ -17,10 +34,11 @@ const FloatingArt = () => {
           ease: "linear",
         } }
       >
-        <Image alt="زخرفة 1" src={ ArtSide } />
-        <Image alt="زخرفة 2" src={ ArtSide } className={ "-my-3" } />
+        <Image alt="زخرفة 1" src={ ArtSide } className="h-[32%]" />
+        <Image alt="زخرفة 2" src={ ArtSide } className={ "-my-3 h-[35%]" } />
       </motion.div>
     </div>
+    </>
   );
 };
 
