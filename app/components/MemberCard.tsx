06a1429ced830042a088645d8600dca82a7c61bc
nizar7zak <nizar7zakout@gmail.com>
2025-03-21 04:12:30 +0200

    edit responsive

--- app/components/MemberCard.tsx
+++ app/components/MemberCard.tsx
@@ -9,11 +9,11 @@ interface Props {
 
 const MemberCard = ( { fullName, src, jobDescription, skillsIcons }: Props ) => {
   return (
-    <div className="flex w-[80%] md:w-[60%] lg:w-[35%] justify-center items-center ">
+    <div className="flex w-[80%] md:gap-5 lg:gap-0 md:w-[70%] lg:w-[35%] justify-center items-center ">
       <div className="w-[24vw] h-[24vw] lg:w-[14vw] lg:h-[14vw] xl:w-[12vw] xl:h-[12vw] rounded-full bg-slate-300 overflow-hidden ml-6">
         <Image alt="team member picture" src={ src } />
       </div>
-      <div className="flex flex-col w-[57%] gap-y-2 xl:gap-y-3 2xl:gap-y-5">
+      <div className="flex flex-col w-[57%] gap-y-2 xl:gap-y-3 2xl:gap-y-5 -mt-6">
         <h1 className="text-active text-base xl:text-xl 2xl:text-2xl font-medium">{ fullName }</h1>
         <h1 className="text-primary text-sm xl:text-base font-light pb-3 border-b-[0.75px] border-primary">{ jobDescription }</h1>
         <div className="flex items-center justify-start gap-2">
