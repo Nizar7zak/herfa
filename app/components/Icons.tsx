4e21b034d3e4f408558d1567c6a785cb84dd8b43
nizar7zak <nizar7zakout@gmail.com>
2025-08-03 19:06:48 +0300

    add whatsapp contact

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -9,13 +9,19 @@ const Icons = () => {
     return (
         <div className="fixed bottom-[2.5rem] left-12 lg:left-0 w-full flex items-center justify-center px-[5.8rem] gap-4 z-50">
             <div className="flex-1 flex justify-start">
-                <Image width={ 50 } alt="whatsapp" src={ Whatsapp } className='cursor-pointer'  />
+                <a
+                    href="https://wa.me/972598687226"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image width={ 50 } alt="whatsapp" src={ Whatsapp } className='cursor-pointer' />
+                </a>
             </div>
             <FaChevronDown size={ 24 } className="text-white hidden lg:block" />
             <div className="flex-1 lg:flex justify-end hidden">
                 <SocialIcons />
             </div>
-        </div> )
+        </div > )
 }
 
 export default Icons
